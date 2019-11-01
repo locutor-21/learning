e04de568b75aae0adb379a3528c818a53299baf2
Pedro Dias <pdcmpa@gmail.com>
2019-11-01 11:49:39 -0300

    feat(diesel): impl write_post

--- diesel/diesel_demo/src/lib.rs
+++ diesel/diesel_demo/src/lib.rs
@@ -5,6 +5,7 @@ extern crate dotenv;
 pub mod models;
 pub mod schema;
 
+use self::models::{NewPost, Post};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
@@ -14,5 +15,17 @@ pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
+        .expect("Error saving new post")
 }
