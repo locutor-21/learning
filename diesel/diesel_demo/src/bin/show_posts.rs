98a6e0d603a423082d702bdab3341fa3a7a4115d
Pedro Dias <pdcmpa@gmail.com>
2019-11-01 11:18:48 -0300

    feat(diesel): initial commit, show_posts script

--- diesel/diesel_demo/src/bin/show_posts.rs
+++ diesel/diesel_demo/src/bin/show_posts.rs
@@ -1,15 +1,16 @@
-extern crate diesel_demo;
 extern crate diesel;
+extern crate diesel_demo;
 
+use self::diesel::prelude::*;
 use self::diesel_demo::*;
 use self::models::*;
-use self::diesel::prelude::*;
 
 fn main() {
     use diesel_demo::schema::posts::dsl::*;
 
     let connection = establish_connection();
-    let results = posts.filter(published.eq(true))
+    let results = posts
+        .filter(published.eq(true))
         .limit(5)
         .load::<Post>(&connection)
         .expect("Error loading posts");
